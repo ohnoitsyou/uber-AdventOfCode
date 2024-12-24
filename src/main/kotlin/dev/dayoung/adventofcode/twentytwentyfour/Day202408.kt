a9dfe11eea21098e04b4dbc961acfa7a2eefbe1b
David Young <ohnoitsyou7043@gmail.com>
2024-12-24 15:43:34 -0500

    Start 24-08; just a shell for now

--- src/main/kotlin/dev/dayoung/adventofcode/twentytwentyfour/Day202408.kt
+++ src/main/kotlin/dev/dayoung/adventofcode/twentytwentyfour/Day202408.kt
@@ -1,4 +1,20 @@
 package dev.dayoung.adventofcode.twentytwentyfour
 
-class Day202408 {
+import dev.dayoung.adventofcode.PuzzleSolution
+import dev.dayoung.adventofcode.Utils
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.stereotype.Component
+
+@Component
+class Day202408: PuzzleSolution(2024, 8, true) {
+    private val log = KotlinLogging.logger {}
+
+    private fun partOne() { }
+
+    private val parser = { input: List<String> -> input }
+
+    override fun solve(sampleMode: Boolean) {
+        log.info { "2024 - Day 08" }
+        val lines = Utils.readInputResource(sampleMode, "2024/eight.txt")?.let { parser(it) }
+    }
 }
