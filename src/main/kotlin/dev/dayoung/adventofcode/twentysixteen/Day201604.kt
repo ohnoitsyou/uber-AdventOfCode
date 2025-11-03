b8d53907ae07524c049a56970b4ce7cf46d4dd94
David Young <ohnoitsyou7043@gmail.com>
2025-11-03 22:16:43 +0000

    Final attempt at part 1
    I was close, but had a lot of help from reddit:
      https://old.reddit.com/r/adventofcode/comments/5gdvve/2016_day_4_solutions/darwrhj/

--- src/main/kotlin/dev/dayoung/adventofcode/twentysixteen/Day201604.kt
+++ src/main/kotlin/dev/dayoung/adventofcode/twentysixteen/Day201604.kt
@@ -19,95 +19,25 @@ class Day201604 : PuzzleSolution(2016, 4) {
 
     private data class Room(val name: String, val sectorId: Int, val checksum: String) {
         fun isValid(): Boolean {
-            return name.replace("-", "").groupBy { it }.mapValues { it.value.size }.toList()
-                .sortedWith(compareBy({0 - it.second }, { it.first }))
+            return name.replace("-", "")
+                .groupBy { it }
+                .mapValues { it.value.size }
+                .toList()
+                .sortedWith(compareBy({0 - it.second }, { it.first })) // Sort second (count) desc, first (letter) asc
                 .take(5)
                 .map { it.first }
                 .joinToString("") == checksum
         }
     }
-    private fun partOne(input: List<String>) {
+    private fun partOne(input: List<String>): Int {
         val roomInput = input.mapNotNull { line ->
             lineRegex.find(line)?.groupValues?.let {
                 Room(it[1], it[2].toInt(10), it[3])
             }
         }
-        val one = roomInput.map { room ->
-            print("Room isValid: ${room.isValid()} -> ")
-            val (name, sectorId, checksum) = room
-            val sanitizedName = name.replace("-", "")
-            val frequencies = sanitizedName.associateWith { c -> sanitizedName.count { it == c } }
-            val fList = frequencies.toList().sortedWith(compareBy({ 0 - it.second }, { it.first }))
-            val topFive = fList.take(5)
-            if (!checksum.all { name.contains(it) }) { return@map 0 }
-            val idxDiffs = checksum.zipWithNext { a, b ->
-                val bidx = fList.indexOfFirst { it.first == b }.takeIf { it >= 0 } ?: -1000
-                val aidx = fList.indexOfFirst { it.first == a }.takeIf { it >= 0 } ?: -100
-                bidx - aidx
-            }
-            val isValid = idxDiffs.all { it > 0 }
-            println("entry: $name-$sectorId[$checksum] : $fList : $idxDiffs")
-            if (isValid) sectorId else 0
-//            sectorId
-        }
-        println("One sum: ${one.sum()}")
-        val two = roomInput.filter { room -> room.isValid() }
-        println("Two sum: $two")
-//        val rm = roomInput.map { (name, sectorId, checksum) ->
-//            val sanitizedName = name.replace("-", "")
-//            val frequencies = sanitizedName.associateWith { c -> sanitizedName.count { it == c } }
-//            val diffs = checksum.zipWithNext { a, b -> frequencies.getOrDefault(a, -1000) - frequencies.getOrDefault(b, -100) }
-//            var isValid = diffs.none { it < 0 }
-//            for((idx, diff) in diffs.withIndex()) {
-//                if(diff > 0) continue
-//                if(diff == 0) {
-//                    val isAlphabetical = checksum[idx] < checksum[idx + 1]
-////                    println("isAlphabetical: $isAlphabetical")
-//                    if(!isAlphabetical) {
-//                        isValid = false
-//                    }
-//                }
-//            }
-//            if(isValid) {
-//                println("sus: $name-$sectorId[$checksum] : ${frequencies.toList().sortedBy { it.second }.reversed()} : $diffs")
-//            }
-//            if(isValid) sectorId else 0
-//        }
-        /*
-        val roomMap = roomInput.map { (name, sectorId, checksum) ->
-            val sanitizedName = name.replace("-", "")
-            val frequencies = sanitizedName.associateWith { c -> sanitizedName.count { it == c } }
-            val frequenciesList = frequencies.toList().sortedBy { (_, count) -> count }.reversed()
-            var last = checksum[0] to frequencies.getOrDefault(checksum[0], 0)
-            val isValid = checksum.drop(1).all { c ->
-                val freq = frequencies.getOrDefault(c, 100)
-                if(last.second > freq) {
-                    true.also {
-                        last = c to freq
-                    }
-                } else if(freq == last.second) {
-                    (last.first < c).also {
-                        last = c to freq
-                    }
-                } else {
-                    println("Failed for reason: last: $last, c: $c, freq: $freq")
-                    false.also {
-                        last = c to freq
-                    }
-                }
-            }
-            if(isValid) sectorId else 0
-        }
-         */
-        val sectorSum = one.sum()
-        println("Sector sum: $sectorSum")
+        val validRooms = roomInput.filter { room -> room.isValid() }
+        return validRooms.sumOf { it.sectorId }
     }
-//    val frequencies = groups[1].replace("-", "").toSet().groupBy {
-//            letter -> groups[1].count { it == letter }
-//    }
-//    val checksum = groups[3]
-//    //                println("Grouped letters: $groupedLetters")
-//    val top5 = frequencies.keys.sortedDescending().take(5)
 
     private fun partTwo(input: String) {
 
@@ -115,66 +45,11 @@ class Day201604 : PuzzleSolution(2016, 4) {
 
     override fun solve(sampleMode: Boolean) {
         Utils.readInputResource(sampleMode, "2016/four.txt")?.let {
-            partOne(it)
+            println("Sector ID sum: ${partOne(it)}")
         }
     }
 }
 
 fun main() {
     Day201604().solve(false)
-}
-
-// WRONG: 139516
-// WRONG: 139200
-// WRONG: 138277
-
-   /*
-//                var last = Triple(true, checksum[0], groups[1].count { it == checksum[0] })
-            val (valid, _, _) = room.checksum.drop(1).fold(Triple(true, room.checksum, room.name.count { it == room.checksum[0] })) { acc, i ->
-                val (valid, c, oldCount) = acc
-                val iCount = room.name.count { it == i }
-                if(oldCount > iCount) {
-                    Triple(valid, i, iCount)
-                } else if(oldCount == iCount) {
-                    if(c < i) {
-                        Triple(valid, i, iCount)
-                    } else {
-                        Triple(false, i, iCount)
-                    }
-                } else {
-                    Triple(false, i, iCount)
-                }
-//                    if(!valid) {
-//                        Triple(false, i, iCount)
-//                    } else if(iCount == count) {
-//                        if(c <= i) {
-//                           Triple(true , i, iCount)
-//                        } else {
-//                           Triple(false, i, iCount)
-//                        }
-//                    } else if(count >= iCount && c < i) {
-//                        Triple(true, i, iCount)
-//                    } else {
-//                        Triple(false, i, iCount)
-//                    }
-                if(valid) {
-                    println("${str} is valid")
-                    sum += sectorId
-                } else {
-                    println("${str} is invalid")
-                }
-                Triple(valid, sum, str)
-            }
-            println("Done: Sum: $sum")
-            output.groupBy { it.first }.forEach { (status, entries) ->
-                println("Status: $status")
-                entries.forEach(::println)
-            }
-        }
-    */
-
-inline fun <T> Iterable<T>.allIndexed(predicate: (Int, T) -> Boolean): Boolean {
-    if (this is Collection && isEmpty()) return true
-    for ((idx, element) in this.withIndex()) if (!predicate(idx, element)) return false
-    return true
 }
