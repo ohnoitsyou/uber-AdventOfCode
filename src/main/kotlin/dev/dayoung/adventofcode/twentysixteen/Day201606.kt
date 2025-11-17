4f6e498e6b877189f013c18edfa1f2d9a2fa1c31
David Young <ohnoitsyou7043@gmail.com>
2025-11-17 21:51:42 +0000

    Add 2016 day 06 solution

---
+++ src/main/kotlin/dev/dayoung/adventofcode/twentysixteen/Day201606.kt
@@ -0,0 +1,35 @@
+package dev.dayoung.adventofcode.twentysixteen
+
+import dev.dayoung.adventofcode.PuzzleSolution
+import dev.dayoung.adventofcode.Utils
+import org.springframework.stereotype.Component
+
+typealias FreqMap = List<Map<Char, Int>>
+
+@Component
+class Day201606 : PuzzleSolution(2016, 6)  {
+    fun freqMap(input: List<String>): FreqMap =
+        (0 until input.first().length)
+            .map { colIndex ->
+                input.map { rowString ->
+                    rowString[colIndex]
+                }.joinToString("")
+            }.map { line ->
+                line.groupBy { it }.mapValues { it.value.size }
+            }
+
+    fun partOne(input: FreqMap): String {
+        return input.map { fMap -> fMap.maxBy { it.value }.key }.joinToString("")
+    }
+    fun partTwo(input: FreqMap): String {
+        return input.map { fMap -> fMap.minBy { it.value }.key }.joinToString("")
+    }
+
+    override fun solve(sampleMode: Boolean) {
+        Utils.readInputResource(sampleMode, "2016/six.txt")?.let { lines ->
+            val freqMap = freqMap(lines)
+            println("Part one: ${partOne(freqMap)}")
+            println("Part two: ${partTwo(freqMap)}")
+        }
+    }
+}
---
+++ src/main/kotlin/dev/dayoung/adventofcode/twentysixteen/Day201607.kt
@@ -0,0 +1,4 @@
+package dev.dayoung.adventofcode.twentysixteen
+
+class Day201607 {
+}
