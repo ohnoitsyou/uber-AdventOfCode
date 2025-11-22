e4c629e58a2d3285ad9be1f5b071962da2ad8282
David Young <ohnoitsyou7043@gmail.com>
2025-11-22 15:29:36 -0500

    Solution to 2016 Day 8

--- src/main/kotlin/dev/dayoung/adventofcode/structures/Grid.kt
+++ src/main/kotlin/dev/dayoung/adventofcode/structures/Grid.kt
@@ -5,25 +5,25 @@ import dev.dayoung.adventofcode.Vec2i
 
 open class Grid<T>(open val points: List<T>, val width: Int, val height: Int, open val oobBehavior: OOBBehavior<T> = Throw()) {
     sealed interface OOBBehavior<T> {
-        fun oob(grid: Grid<T>, point: Vec2i): T
+        fun oob(grid: Grid<T>, point: Vec2i): Pair<Vec2i, T>
     }
     class Throw<T> : OOBBehavior<T> {
-        override fun oob(grid: Grid<T>, point: Vec2i): T {
+        override fun oob(grid: Grid<T>, point: Vec2i): Pair<Vec2i, T> {
             throw IndexOutOfBoundsException("Out of bounds")
         }
     }
 
     class ReturnDefault<T>(private val default: T): OOBBehavior<T> {
-        override fun oob(grid: Grid<T>, point: Vec2i): T {
-            return default
+        override fun oob(grid: Grid<T>, point: Vec2i): Pair<Vec2i, T> {
+            return point to default
         }
     }
 
     class Loop<T>(): OOBBehavior<T> {
-        override fun oob(grid: Grid<T>, point: Vec2i): T {
+        override fun oob(grid: Grid<T>, point: Vec2i): Pair<Vec2i, T> {
             val x = point.x % grid.width
             val y = point.y % grid.height
-            return grid[x, y]
+            return Vec2i(x, y) to grid[x, y]
         }
     }
 
@@ -48,7 +48,9 @@ open class Grid<T>(open val points: List<T>, val width: Int, val height: Int, op
     operator fun get(x: Int, y: Int): T {
         return if (x in 0 until width && y in 0 until height) {
             points[y * width + x]
-        } else oobBehavior.oob(this, Vec2i(x, y))
+        } else {
+          oobBehavior.oob(this, Vec2i(x, y)).second
+        }
     }
     operator fun get(c: Vec2i): T = get(c.x, c.y)
 
@@ -99,7 +101,13 @@ class MutableGrid<T>(override val points: MutableList<T>, width: Int, height : I
         if (p <= this.points.lastIndex) {
             points[p] = value
         } else {
-            points[oobBehavior.oob(this, point)]
+          if (oobBehavior is Loop) {
+            val loopPoint: Vec2i = oobBehavior.oob(this, Vec2i(p, p)).first
+            val idx = loopPoint.y * width + loopPoint.x
+            points[idx] = value
+          } else {
+            oobBehavior.oob(this, Vec2i(p, p))
+          }
         }
     }
 }
@@ -113,25 +121,23 @@ fun Grid<Char>.withVisited(visited: Set<Vec2i>, visitChar: Char = 'X'): Grid<Cha
 }
 
 fun MutableGrid<Char>.setRectangle(p1: Vec2i, p2: Vec2i, char: Char) {
-    val minx = minOf(p1.x, p2.x)
-    val miny = minOf(p1.y, p2.y)
-    val maxx = maxOf(p1.x, p2.x)
-    val maxy = maxOf(p1.y, p2.y)
-    (minx .. maxx).forEach { x ->
-        (miny .. maxy).forEach { y ->
+    (minOf(p1.x, p2.x) .. maxOf(p1.x, p2.x)).forEach { x ->
+        (minOf(p1.y, p2.y) .. maxOf(p1.y, p2.y)).forEach { y ->
             this[Vec2i(x, y)] = char
         }
     }
 }
 
 fun MutableGrid<Char>.rotateColumn(x: Int, depth: Int) {
-    val src = Grid.Column<Char>(this, x)
-    src.cells.map {
-        val newPoint = it.first + Vec2i(0, depth)
-        println("Mapped ${it.first.x}, ${it.first.y} : (${it.second}) to ${newPoint.x}, ${newPoint.y} : (${it.second})")
-        it.first + Vec2i(0, depth) to it.second
-    }.forEach {
+    Grid.Column(this, x).cells.forEach { (point, value) ->
+        val wrappedPoint = Vec2i(point.x, (point + Vec2i(0, depth)).y % height)
+        this[wrappedPoint] = value
+    }
+}
 
-        this[it.first] = it.second
+fun MutableGrid<Char>.rotateRow(y: Int, depth: Int) {
+    Grid.Row(this, y).cells.forEach { (point, value) ->
+        val wrappedPoint = Vec2i((point + Vec2i(depth, 0)).x % width, point.y)
+        this[wrappedPoint] = value
     }
 }
