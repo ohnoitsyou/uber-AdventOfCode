328cfacb4c1e81947c8a708b7e6c843914aab6bc
David Young <ohnoitsyou7043@gmail.com>
2025-12-04 02:38:22 -0500

    Solution to 2025 Day 3 pt2
    When I went to go find some hints on the subreddit, I found someone's
    visualization of the solution and the whole thing clicked

--- src/main/kotlin/dev/dayoung/adventofcode/twentytwentyfive/Day202503.kt
+++ src/main/kotlin/dev/dayoung/adventofcode/twentytwentyfive/Day202503.kt
@@ -2,12 +2,10 @@ package dev.dayoung.adventofcode.twentytwentyfive
 
 import dev.dayoung.adventofcode.PuzzleSolution
 import dev.dayoung.adventofcode.Utils
+import org.springframework.stereotype.Component
 
+@Component
 class Day202503: PuzzleSolution(2025, 3) {
-    fun maxWithTail(bank: List<Int>): Pair<Int, List<Int>> {
-        return bank.max().let { it to bank.subList(bank.indexOf(it) + 1, bank.lastIndex) }
-    }
-
     fun partOne(input: List<String>): Int {
         return input.sumOf { bank ->
             (0..bank.lastIndex).flatMap { i ->
@@ -18,40 +16,28 @@ class Day202503: PuzzleSolution(2025, 3) {
         }
     }
 
-    fun nextHighestBeforeLower(bank: List<Int>): Pair<Int, List<Int>> {
-        val windows = bank.windowed(2)
-        return windows.indexOfFirst { it[0] >= it[1] || it[0] == it[1] }.let { if(bank.lastIndex > it + 1) bank[it] to bank.drop(it + 1) else bank[it] * 10 + bank[it + 1] to emptyList() }
-    }
-
     fun partTwo(input: List<String>): Long {
-        input[2].let { str ->
-            var digitList = str.map { it.digitToInt() }
-            buildString {
-                repeat(12) {
-                    val (p1, b1) = nextHighestBeforeLower(digitList)
-                    append(p1)
-                    digitList = b1
+        val banksOn = input.map { bank ->
+            bank.map { it.digitToInt() }.let { str: List<Int> ->
+                val on = mutableListOf<Int>()
+                val candidates = str.toMutableList()
+                while (on.size < 12) {
+                    val cur = candidates.dropLast(11 - on.size)
+                    val max = cur.max()
+                    on.add(max)
+                    val maxIdx = cur.indexOf(max)
+                    candidates.subList(0, maxIdx + 1).clear()
                 }
-            }.also { log.info { it } }
-//            val (p2, b2) = nextHighestBeforeLower(b1)
-//            log.info { "p1: $p1, b1: $b1, p2: $p2, b2: $b2" }
+                on
+            }.joinToString("").toLong()
         }
-//        val banks: List<String> = input.map { bank ->
-//            var b: List<Int> = bank.map { it.digitToInt() }
-//            ""
-//        }
-//        log.info { "Banks: $banks" }
-        return 0
+        return banksOn.sum()
     }
 
     override fun solve(sampleMode: Boolean) {
         Utils.readInputResource("2025/03.txt", sampleMode)?.let { input ->
-//            log.info { "Part One: ${partOne(input)}" }
+            log.info { "Part One: ${partOne(input)}" }
             log.info { "Part Two: ${partTwo(input)}" }
         }
     }
-}
-
-fun main() {
-    Day202503().solve(true)
 }
