75577c2f31f50aff85125e8beb5e4a8c2e3b001e
David Young <ohnoitsyou7043@gmail.com>
2025-12-10 01:09:09 -0500

    Tidy up the code for 2025 07 a bit

--- src/main/kotlin/dev/dayoung/adventofcode/twentytwentyfive/Day202507.kt
+++ src/main/kotlin/dev/dayoung/adventofcode/twentytwentyfive/Day202507.kt
@@ -4,118 +4,84 @@ import dev.dayoung.adventofcode.PuzzleSolution
 import dev.dayoung.adventofcode.Utils
 import dev.dayoung.adventofcode.Vec2i
 import dev.dayoung.adventofcode.structures.Grid
-import dev.dayoung.adventofcode.structures.MutableGrid
+import org.springframework.stereotype.Component
 
+@Component
 class Day202507: PuzzleSolution(2025, 7) {
-    fun partOne(grid: MutableGrid<Char>): Int {
+    fun partOne(grid: Grid<Char>): Int {
         // find the start of the "tree"
         val start = grid.points.indexOf('S')
         var row = 0
         var splitCount = 0
-        var beams = mutableSetOf(Vec2i(start, 0))
+        val beams = mutableSetOf(Vec2i(start, 0))
         while(row < grid.height - 1) {
             beams.filter { it.y == row }.forEach { beam ->
                 val np = beam + Vec2i.DOWN
                 if( grid[np] == '.') {
-                    grid[np] = '|'
                     beams.add(np)
                 } else if(grid[np] == '^') {
                     splitCount++
-                    grid[np + Vec2i.LEFT] = '|'
-                    grid[np + Vec2i.RIGHT] = '|'
                     beams.addAll(listOf(np + Vec2i.LEFT, np + Vec2i.RIGHT))
                 }
             }
-            grid.print()
             row++
         }
-        grid.print()
         return splitCount
     }
 
-    // This implementation works from the top down, which is super inefficient because you are following several paths many times.
-    // A better solution would be to find all the splits, work from bottom up and for each split, add the number of previous destinations.
-    // Then the value at the top should be the final count
-    // This implementation also OOM'd
-    fun partTwo(grid: MutableGrid<Char>): Int {
-        val start = Vec2i(grid.points.indexOf('S'), 0)
-        val column = grid[start.x]
-        val splitY = column.values.indexOfFirst { it == '^' }
-        val firstSplit = Vec2i(start.x, splitY)
-        val stack = ArrayDeque(listOf(firstSplit + Vec2i.LEFT, firstSplit + Vec2i.RIGHT))
-        var timelines = 0
-        while(stack.isNotEmpty()) {
-            var current = stack.removeFirst()
-            while(grid[current] != '^' && current.y <= grid.height) {
-                grid[current] = '|'
-                current += Vec2i.DOWN
-            }
-            if(grid[current] == '^') {
-                stack.addAll(listOf(current + Vec2i.LEFT, current + Vec2i.RIGHT))
-            } else {
-                // must have reached the bottom
-                timelines++.logit("Bottom count")
-            }
+    // This logic could probably use some work, but I'm ok with it for now.
+    fun findParentPoint(grid: Grid<Char>, cache: MutableMap<Vec2i, Long>, parent: Vec2i, start: Vec2i) {
+        var p = start
+        while(!cache.keys.contains(p) && p.y <= grid.height) {
+            p += Vec2i.DOWN
         }
-        return timelines
+        if(cache.keys.contains(p)) {
+            val nvL = cache.getOrPut(parent) { 0 }
+            cache[parent] = nvL + cache[p]!!
+        } else {
+            // we've hit the bottom
+            val nvL = cache.getOrDefault(parent, 0)
+            cache[parent] = nvL + 1
+        }
     }
 
-    fun partTwoReverse(grid: Grid<Char>): Int {
-        grid.getRow(grid.height - 2).cells.logit()
-        // knowing that the last row is full dots...
+    /**
+     * Start at the bottom of the tree. Seed the cache with the last row of splits,
+     *   each of those has two paths to the bottom
+     * Moving up the tree, find each split and trace it back down until we either find a split or the bottom.
+     * If we find a split, take the value of that split from the cache and store it on ourselves.
+     * Do this for both left and right of the original split.
+     * If we hit the bottom, use the value 1.
+     * This cumulative value means we don't have to track back to the root every time.
+     * The final number of 'timelines' is the value from the cache node near the top of the tree, the first split.
+     */
+    fun partTwo(grid: Grid<Char>): Long {
         val splitCache = mutableMapOf<Vec2i, Long>()
-        // seed the cache with the first row
+        // knowing that the last row is full dots...
+        // seed the cache with the second to last row
         grid.getRow(grid.height - 2).cells.forEach { (point, _) ->
             if(grid[point] == '^') {
                 splitCache.getOrPut(point) { 2 }
             }
         }
         for(row in grid.height - 3 downTo 0) {
-            val r = grid.getRow(row)
-            r.cells.forEach {(point, v) ->
+            grid.getRow(row).cells.forEach {(point, v) ->
                 if(v == '^') {
-                    var left = point + Vec2i.LEFT
-                    while(!splitCache.keys.contains(left) && left.y <= grid.height) {
-                        left += Vec2i.DOWN
-                    }
-                    if(splitCache.keys.contains(left)) {
-                        left.logit("Found previous left point")
-                        val nvL = splitCache.getOrPut(point) { 0 }
-                        splitCache[point] = nvL + splitCache[left]!!
-                    } else {
-                        // we've hit the bottom
-                        val nvL = splitCache.getOrDefault(point, 0)
-                        splitCache[point] = nvL + 1
-                    }
+                    val left = point + Vec2i.LEFT
+                    findParentPoint(grid, splitCache, point, left)
 
-                    var right = point + Vec2i.RIGHT
-                    while(!splitCache.keys.contains(right) && right.y <= grid.height) {
-                        right += Vec2i.DOWN
-                    }
-                    if(splitCache.keys.contains(right)) {
-                        right.logit("Found previous right point")
-                        val nvR = splitCache.getOrPut(point) { 0 }
-                        splitCache[point] = nvR + splitCache[right]!!
-                    } else {
-                        // we've hit the bottom
-                        val nvR = splitCache.getOrDefault(point, 0)
-                        splitCache[point] = nvR + 1
-                    }
-                } else if(v == 'S') {
-                    splitCache.logit("Final cache")
+                    val right = point + Vec2i.RIGHT
+                    findParentPoint(grid, splitCache, point, right)
                 }
             }
         }
-
-        return 0
+        return splitCache.entries.minBy { (vec, _) -> vec.y }.value
     }
 
     override fun solve(sampleMode: Boolean) {
         Utils.readInputResource("2025/07.txt", sampleMode)?.let { input ->
-//            partOne(MutableGrid(input.joinToString("").toMutableList(), input.first().length, input.size)).logit("Split count")
-            partTwoReverse(MutableGrid(input.joinToString("").toMutableList(), input.first().length, input.size, Grid.ReturnDefault('.'))).logit("Timeline count")
+            partOne(Grid(input.joinToString("").toMutableList(), input.first().length, input.size)).logit("Split count")
+            partTwo(Grid(input.joinToString("").toMutableList(), input.first().length, input.size, Grid.ReturnDefault('.'))).logit("Timeline count")
         }
     }
 }
-
-fun main() = Day202507().solve(false)
