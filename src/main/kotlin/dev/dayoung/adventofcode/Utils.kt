0b858f2670518c798d82cd3a9580997531c78f5c
David Young <ohnoitsyou7043@gmail.com>
2025-12-10 19:52:49 -0500

    Fixup after merge

--- src/main/kotlin/dev/dayoung/adventofcode/Utils.kt
+++ src/main/kotlin/dev/dayoung/adventofcode/Utils.kt
@@ -100,19 +100,23 @@ fun List<String>.toLongRanges(sep: String = "-"): List<LongRange> {
     return map { LongRange(it.substringBefore(sep).toLong(), it.substringAfter(sep).toLong()) }
 }
 
-
 data class Vec3i(val x: Int, val y: Int, val z: Int) {
+    operator fun plus(other: Vec3i): Vec3i { return Vec3i(x + other.x, y + other.y, z + other.z) }
+    operator fun minus(other: Vec3i): Vec3i { return Vec3i(x - other.x, y - other.y, z - other.z) }
+
     fun eDistance(other: Vec3i): Double {
         val dx = (x - other.x).toDouble()
         val dy = (y - other.y).toDouble()
         val dz = (z - other.z).toDouble()
         return sqrt(dx.pow(2) + dy.pow(2) + dz.pow(2))
     }
+
+    override fun toString(): String = "($x, $y, $z)"
 }
 
-fun List<String>.toVec3iList(sep: String = ",") : List<Vec3i> {
+fun List<String>.toVec3iList(sep: String = ","): List<Vec3i> {
     return map {
-        val (x, y, z) = it.split(",")
+        val (x, y, z) = it.split(sep)
         Vec3i(x.toInt(), y.toInt(), z.toInt())
     }
 }
